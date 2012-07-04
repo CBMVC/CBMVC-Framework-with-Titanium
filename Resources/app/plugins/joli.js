cc46f65704ceeb1eca85dafc0fd130d2dcba9341
winson <winsonet@gmail.com>
2012-07-04 23:02:55 +0800

    fixed joli's bug
    cannot get the correct array type

--- Resources/app/plugins/joli.js
+++ Resources/app/plugins/joli.js
@@ -36,9 +36,12 @@ var joli = {
         }
     },
     getType: function(obj) {
+    	var funcNameRegex = /function (.{1,})\(/;
+   		
         if (typeof obj === "undefined" || obj === null || (typeof obj === "number" && isNaN(obj))) {
             return false;
-        } else if (obj.constructor === Array) {
+        //fixed the array type checking by winson 20120702
+        } else if ((funcNameRegex).exec(obj.constructor.toString())[1]  === "Array") {
             return "array";
         } else {
             return typeof obj;
